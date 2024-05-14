b3e75ecac97fd2378e1238f2f6b8f61b5f7279a2
Nick Vigier <nvigier46@gmail.com>
2024-05-14 07:37:19 +0100

    Add fetch with revalidate, still uses SSR

--- apps/dapp/src/app/markets/page.tsx
+++ apps/dapp/src/app/markets/page.tsx
@@ -1,9 +1,18 @@
 export const dynamic = 'force-dynamic';
 
-export default function MarketsPage() {
+export default async function MarketsPage() {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    next: {
+      revalidate: 2000,
+    }
+  }).then(
+    (response) => response.json()
+  );
+
   return (
     <div>
       <h1>Markets</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
